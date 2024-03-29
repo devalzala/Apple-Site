54bbd6e1cd5b2307d7983911e5e5e941eddb9420
Devalsinh Zala <84832101+devalzala@users.noreply.github.com>
2024-03-29 12:51:25 +0530

    Add files via upload

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Highlights = () => {
+  return <section id="highlights" className="w-screen overflow-hidden h-full common-padding">Highlights</section>;
+};
+
+export default Highlights;
